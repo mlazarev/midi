2e72e2a65dcc1ac1466db7826a72d11a00c136b1
Mike Lazarev <mike.lazarev@gmail.com>
2025-10-19 17:48:00 +0100

    Untrack Windows batch files per .gitignore (*.bat)

--- send_factory.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "REPO=%~dp0"
-python "%REPO%implementations\korg\ms2000\tools\send_to_ms2000.py" --out 6 --file "%REPO%implementations\korg\ms2000\patches\factory\FactoryBanks.syx"
-endlocal
